43fdd7082732eb0f4d24a4b27c46feb65f5a2846
Reeso <jerry.huang@mail.polimi.it>
2024-03-25 21:57:24 +0100

    Partially implemented PlayArea and Player classes

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Player/PlayArea.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Player/PlayArea.java
@@ -1,7 +1,9 @@
 package it.polimi.ingsw.model.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 import it.polimi.ingsw.model.Card.EvaluableCard;
 import it.polimi.ingsw.model.Card.PlayableCard;
@@ -11,6 +13,9 @@ import it.polimi.ingsw.utils.Coordinate;
 
 public class PlayArea {
 
+    /**
+     *The player's hand contains the cards the player can play
+     */
     private List<PlayableCard> hand;
 
     private Map<Coordinate, Card> playedCards;
@@ -19,11 +24,49 @@ public class PlayArea {
 
     private EvaluableCard selectedCard;
 
-    public void selectCard(Card c) {
+    public PlayArea(){
 
+        /*It should have a parameter that builds the player's hand,
+        do I build the cards in here or is it the game?'s responsability*/
+        this.hand = new ArrayList<>();
+
+        this.playedCards = new HashMap<>();
+
+        this.uncoveredItems = new HashMap<>();
+            uncoveredItems.put(Item.PLANT, 0);
+            uncoveredItems.put(Item.ANIMAL, 0);
+            uncoveredItems.put(Item.FUNGI, 0);
+            uncoveredItems.put(Item.INSECT, 0);
+            uncoveredItems.put(Item.QUILL, 0);
+            uncoveredItems.put(Item.INKWELL, 0);
+            uncoveredItems.put(Item.MANUSCRIPT, 0);
+
+        this.selectedCard = null;
     }
 
+    public List<PlayableCard> getHand(){
+        return this.hand;
+    }
+
+    public Map<Coordinate, Card> getPlayedCards(){
+        return this.playedCards;
+    }
+
+    public Map<Item, Integer> getUncoveredItems(){
+        return this.uncoveredItems;
+    }
+
+    public void selectCard(Card c) {
+        this.selectedCard = (EvaluableCard) c;
+    }
+
+    /**
+     *
+     * @param c the card that needs to be checked for constraints
+     * @return true if the player can play it, false if the player can't play it
+     */
     public boolean checkConstraint(PlayableCard c) {
+
         return false;
     }
 
@@ -48,7 +91,8 @@ public class PlayArea {
     }
 
     public void addToHand(PlayableCard c) {
-
+        hand.add(c);
+        //TODO handle exceptions (ie hand is bigger or equal to 3 cards)
     }
 
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Player/Player.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Player/Player.java
@@ -2,31 +2,122 @@ package it.polimi.ingsw.model.Player;
 
 import it.polimi.ingsw.model.Card.ObjectiveCard;
 import it.polimi.ingsw.model.Card.StartCard;
+import it.polimi.ingsw.utils.Coordinate;
 
 import java.util.List;
 
+/**
+ * A class to represent a player in-game
+ */
 public class Player {
 
+    /**
+     * The player's username
+     */
     private String username;
 
+    /**
+     * The player's colored token which can be either red, blue, green or yellow
+     */
     private Token token;
 
+    /**
+     * The player's own playArea which contains all the data and methods regarding the game's execution
+     */
     private PlayArea playArea;
 
+    /**
+     * The player's personal objective
+     */
     private ObjectiveCard secretObjective;
 
+    /**
+     * The player's connection status
+     */
     private boolean online;
 
+    /**
+     * Player constructor
+     * @param username string given to set the player's username
+     */
+    public Player(String username){
+        this.username = username;
+        this.online = true;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public Token getToken(){
+        return this.token;
+    }
+
+    public boolean getStatus(){
+        return this.online;
+    }
+
+    public PlayArea getPlayArea(){
+        return this.playArea;
+    }
+
+    public ObjectiveCard getSecretObjective(){
+        return this.secretObjective;
+    }
+
+
+
+    /**
+     * Initializes player's playArea
+     * @param c the startCard that a player selects upon starting the game
+     */
     public PlayArea start(StartCard c) {
-        return null;
+
+        /*clone of the paramater c into startCard if needed
+        StartCard startCard = new StartCard();
+        startCard.id = c.getID();
+        startCard.backPermanentResources = c.getBackPermanentResources();
+        startCard.corners = c.getCorners();
+        startCard.backCorners = c.getBackCorners();
+        startCard.flipped = c.isFlipped();
+         */
+
+
+        Coordinate coordinate = new Coordinate(0, 0);
+        PlayArea playArea = new PlayArea();
+        playArea.getPlayedCards().put(coordinate, c);
+
+        if(c.isFlipped()){
+
+        }
+        else{
+
+        }
+
+
+        return playArea;
     }
 
+
+    /**
+     * Shows the untaken tokens and then assigns the chosen token to the player
+     * @param t the list of available tokens a player can choose from
+     */
     public void chooseToken(List<Token> t) {
-
+        //TODO needs the player's controller
     }
 
-    public void chooseSecret(ObjectiveCard a, ObjectiveCard b) {
+    //*second possible way to implement chooseToken*
+    /*il game dovrebbe assegnare il token dei players*/
 
+
+    /**
+     * Assigns a secret objectiveCard to the player which other players can't see
+     * @param a one of the two possible objectiveCard the player can choose at the start
+     * @param b one of the two possible objectiveCard the player can choose at the start
+     */
+    public void chooseSecret(ObjectiveCard a, ObjectiveCard b) {
+        //TODO needs the player's controller
     }
 
 }
