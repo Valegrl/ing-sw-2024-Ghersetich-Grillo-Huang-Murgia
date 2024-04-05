659313985b958516244b03bce08a9256d7a17ff4
Valerio Grillo <valerio.grillo@mail.polimi.it>
2024-04-05 16:18:11 +0200

    new CornerEvaluator implementation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Evaluator/CornerEvaluator.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Evaluator/CornerEvaluator.java
@@ -1,10 +1,12 @@
 package it.polimi.ingsw.model.Evaluator;
 
 import it.polimi.ingsw.model.Card.EvaluableCard;
+import it.polimi.ingsw.model.Card.PlayableCard;
 import it.polimi.ingsw.model.Player.PlayArea;
-import it.polimi.ingsw.model.exceptions.NoCoveredCardsException;
 import it.polimi.ingsw.utils.Coordinate;
 import it.polimi.ingsw.utils.Pair;
+
+import java.util.Map;
 
 /**
  * A class that evaluates a card that gives points based on the corners it covers.
@@ -12,15 +14,26 @@ import it.polimi.ingsw.utils.Pair;
 public class CornerEvaluator extends Evaluator {
 
     @Override
-    public int calculatePoints(PlayArea playArea) throws NoCoveredCardsException {
+    public int calculatePoints(PlayArea playArea) {
 
         int countCorners = 0;
 
-        Pair<Coordinate, EvaluableCard> card = playArea.getSelectedCard();
-        Coordinate[] corners = playArea.newlyCoveredCards(card.getKey());
+        Coordinate[] corners = {
+                new Coordinate(-1,1),
+                new Coordinate(1,1),
+                new Coordinate(1,-1),
+                new Coordinate(-1,-1)
+        };
+        Coordinate start = new Coordinate(0,0);
+        Coordinate check;
 
-        for (Coordinate c : corners){
-            if(c!=null)
+        Pair<Coordinate, EvaluableCard> card = playArea.getSelectedCard();
+        Map<Coordinate, PlayableCard> playedCards = playArea.getPlayedCards();
+
+
+        for (Coordinate corner : corners) {
+            check = card.getKey().add(corner);
+            if (check.equals(start) || playedCards.containsKey(check))
                 countCorners++;
         }
 
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Evaluator/Evaluator.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Evaluator/Evaluator.java
@@ -14,6 +14,6 @@ public abstract class Evaluator {
      * @param playArea Where points are calculated.
      * @return Calculated points.
      */
-    public abstract int calculatePoints(PlayArea playArea) throws NoCoveredCardsException;
+    public abstract int calculatePoints(PlayArea playArea);
 
 }
