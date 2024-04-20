82cbd6e3b19800f5515cf914c24e9949aa9cb494
Valerio Grillo <valerio.grillo@mail.polimi.it>
2024-04-20 16:48:18 +0200

    methods for controller package

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/Controller.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -1,17 +1,30 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.listener.GameListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This is a Singleton class that manages the game and lobby controllers.
- * It maintains a list of all active game controllers and lobby controllers.
+ * The {@code Controller} class is a Singleton that serves as a central point of control for managing game and lobby controllers.
+ * It maintains a list of all active {@code GameController} and {@code LobbyController} instances.
  *
- * <p>Use {@link #getInstance()} to get the instance of this class.</p>
+ * <p>The class provides methods to create and join lobbies, reconnect to a game, and retrieve lists of available lobbies and games.
+ * These operations are synchronized to ensure thread safety.</p>
+ *
+ * <p>Use the {@link #getInstance()} method to get the single instance of this class.</p>
+ *
+ * <p>Each {@code LobbyController} and {@code GameController} is identified by a unique identifier, which can be
+ * retrieved using their {@code getIdentifier()} methods. These identifiers are used to manage and track the controllers.
+ * The identifier is unique across both games and lobbies.
+ * Once a lobby is ready to start a game, the same identifier is used for the game, and the lobby is deleted.</p>
+ *
+ * <p>This class is part of the {@code it.polimi.ingsw.controller} package, which contains the main classes for
+ * controlling the game flow.</p>
  */
 public class Controller {
+    //TODO review synchronized methods
     private final static Controller controller = new Controller();
-
     private final List<GameController> gameControllers;
     private final List<LobbyController> lobbyControllers;
 
@@ -25,11 +38,120 @@ public class Controller {
     }
 
     /**
+     * Creates a new lobby and returns the corresponding {@code LobbyController}.
+     *
+     * <p>The lobby is identified by a unique identifier, which is also used for the game once the lobby is ready to start.
+     * The lobby is then deleted.</p>
+     *
+     * @param listener The {@code GameListener} to be associated with the lobby.
+     * @param playerName The name of the player creating the lobby.
+     * @param lobbyID The unique identifier for the lobby.
+     * @param nPlayersRequired The number of players required to start the game.
+     * @return The {@code LobbyController} for the newly created lobby.
+     */
+    public synchronized LobbyController createLobby(GameListener listener, String playerName, String lobbyID, int nPlayersRequired){
+        //TODO implementation
+        /*check if lobbyID is unique*/
+    }
+
+    /**
+     * Allows a player to join an existing lobby and returns the corresponding {@code LobbyController}.
+     *
+     *<p>The lobby is identified by a unique identifier, which is also used for the game once the lobby is ready to start.
+     * The lobby is then deleted.</p>
+     *
+     * @param listener The {@code GameListener} to be associated with the lobby.
+     * @param playerName The name of the player joining the lobby.
+     * @param lobbyID The unique identifier for the lobby.
+     * @return The {@code LobbyController} for the lobby the player joined.
+     */
+    public synchronized LobbyController joinLobby(GameListener listener, String playerName, String lobbyID){
+        //TODO implementation
+    }
+
+    /**
+     * Allows a player to reconnect to an existing game and returns the corresponding {@code GameController}.
+     * Only players who were present when the game started can reconnect.
+     *
+     * @param listener The {@code GameListener} to be associated with the game.
+     * @param playerName The name of the player reconnecting to the game.
+     * @param gameID The unique identifier for the game.
+     * @return The {@code GameController} for the game the player reconnected to.
+     */
+    public synchronized GameController reconnectToGame(GameListener listener, String playerName, String gameID){
+        //TODO implementation
+    }
+
+    /**
+     * Retrieves a list of identifiers for all available lobbies.
+     *
+     * @return A list of identifiers for all available lobbies.
+     */
+    public synchronized List<String> getLobbiesAvailable(){
+        List<String> lobbies = new ArrayList<>();
+
+        for(LobbyController lobby : lobbyControllers)
+            lobbies.add(lobby.getIdentifier());
+
+        return lobbies;
+    }
+
+    /**
+     * Retrieves a list of identifiers for all available games.
+     *
+     * @return A list of identifiers for all available games.
+     */
+    public synchronized List<String> getGamesAvailable(){
+        List<String> games = new ArrayList<>();
+
+        for(GameController game : gameControllers)
+            games.add(game.getIdentifier());
+
+        return games;
+    }
+
+    /**
      * Returns the single instance of the Controller class.
      *
      * @return the instance of the Controller class.
      */
     public static Controller getInstance(){
         return controller;
+    }
+
+    /**
+     * Adds a LobbyController to the list of active lobby controllers.
+     *
+     * @param lc The LobbyController to be added.
+     */
+    protected synchronized void addToLobbyControllers(LobbyController lc){
+        lobbyControllers.add(lc);
+    }
+
+    /**
+     * Removes a LobbyController from the list of active lobby controllers.
+     *
+     * @param lc The LobbyController to be removed.
+     */
+    protected synchronized void removeFromLobbyControllers(LobbyController lc){
+        lobbyControllers.remove(lc);
+    }
+
+    /**
+     * Adds a GameController to the list of active game controllers.
+     *
+     * @param gc The GameController to be added.
+     */
+    protected synchronized void addToGameControllers(GameController gc){
+        gameControllers.add(gc);
+    }
+
+    /**
+     * Removes a GameController from the list of active game controllers.
+     *
+     * @param gc The GameController to be removed.
+     */
+    protected synchronized void removeFromGameControllers(GameController gc){
+        gameControllers.remove(gc);
     }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/GameController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -1,8 +1,61 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Lobby;
+
 /**
  * This class is responsible for managing the game logic.
  * It interacts with the model and view components to control the flow of the game.
+ *
+ * <p>Each instance of this class corresponds to a single game in the application.
+ * The game is identified by a unique identifier, which is also used for the lobby from which the game was started.</p>
+ *
+ * <p>The class provides methods to delete a game and retrieve information about the game.</p>
+ *
+ * <p>The {@code GameController} is created with a {@code Lobby}, which is used to start the game and retrieve the game settings.
+ * The {@code GameController} is then added to the list of active game controllers in the {@code Controller} singleton.</p>
+ *
+ * <p>This class is part of the {@code it.polimi.ingsw.controller} package, which contains the main classes for controlling
+ * the game flow.</p>
  */
 public class GameController {
+    //TODO review class, implementation, listeners and synchronized methods
+    /**
+     * The Game instance that this controller manages.
+     * It contains the state of the game, including the list of players, the game board, and the game settings.
+     */
+    private final Game game;
+
+    /**
+     * Constructs a new GameController with the given lobby.
+     * It starts the game in the lobby, creates a new Game instance with the game settings from the lobby, and adds
+     * this controller to the list of active game controllers in the Controller singleton.
+     *
+     * @param lobby The Lobby from which the game is started.
+     */
+    public GameController(Lobby lobby) {
+        this.game = lobby.startGame();
+        Controller.getInstance().addToGameControllers(this);
+    }
+
+    /**
+     * Deletes the game.
+     * This method deletes the Game instance that this controller manages and removes this GameController from the
+     * list of active game controllers in the Controller singleton.
+     */
+    protected synchronized void deleteGame(){
+
+    }
+
+    /**
+     * Retrieves the unique identifier of the game.
+     * This method retrieves the unique identifier of the Game instance that this controller manages.
+     *
+     * @return The unique identifier of the game.
+     */
+    public synchronized String getIdentifier(){
+        return game.getId();
+    }
+
+    //TODO Game logic and TurnManager
 }
