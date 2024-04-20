c00374bd2ffe1821df258b271e5612c77a11d5b3
Valerio Grillo <valerio.grillo@mail.polimi.it>
2024-04-20 11:53:39 +0200

    Identifier changed from int to String

--- CodexNaturalis/src/test/java/it/polimi/ingsw/model/LobbyTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/model/LobbyTest.java
@@ -11,14 +11,14 @@ class LobbyTest {
 
     @Test
     void addPlayerToLobbySuccessfully() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         assertDoesNotThrow(() -> lobby.addPlayer("player2"));
         assertEquals(2, lobby.getJoinedPlayers().size());
     }
 
     @Test
     void addPlayerToFullLobbyThrowsException() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         try {
             lobby.addPlayer("player2");
         } catch (NonUniqueUsernameException | FullLobbyException e) {
@@ -29,13 +29,13 @@ class LobbyTest {
 
     @Test
     void addPlayerWithDuplicateUsernameThrowsException() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         assertThrows(NonUniqueUsernameException.class, () -> lobby.addPlayer("player1"));
     }
 
     @Test
     void removePlayerFromLobbySuccessfully() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         try {
             lobby.addPlayer("player2");
         } catch (NonUniqueUsernameException | FullLobbyException e) {
@@ -47,13 +47,13 @@ class LobbyTest {
 
     @Test
     void removeNonExistentPlayerFromLobby() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         assertFalse(lobby.removePlayer("player2"));
     }
 
     @Test
     void startGameWithSufficientPlayers() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         try {
             lobby.addPlayer("player2");
         } catch (NonUniqueUsernameException | FullLobbyException e) {
@@ -68,36 +68,36 @@ class LobbyTest {
 
     @Test
     void startGameWithInsufficientPlayersThrowsException() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         assertThrows(InsufficientPlayersException.class, lobby::startGame);
     }
 
     @Test
     void lobbyConstructorThrowsExceptionWhenUsernameIsNull() {
-        assertThrows(NullPointerException.class, () -> new Lobby(1, null, 2));
+        assertThrows(NullPointerException.class, () -> new Lobby("NameID", null, 2));
     }
 
     @Test
     void lobbyConstructorThrowsExceptionWhenRequiredPlayersIsInvalid() {
-        assertThrows(IllegalArgumentException.class, () -> new Lobby(1, "player1", 5));
-        assertThrows(IllegalArgumentException.class, () -> new Lobby(1, "player1", 1));
+        assertThrows(IllegalArgumentException.class, () -> new Lobby("NameID", "player1", 5));
+        assertThrows(IllegalArgumentException.class, () -> new Lobby("NameID", "player1", 1));
     }
 
     @Test
     void addPlayerThrowsExceptionWhenUsernameIsNull() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         assertThrows(NullPointerException.class, () -> lobby.addPlayer(null));
     }
 
     @Test
     void removePlayerThrowsExceptionWhenUsernameIsNull() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         assertThrows(NullPointerException.class, () -> lobby.removePlayer(null));
     }
 
     @Test
     void getJoinedPlayers() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         try {
             lobby.addPlayer("player2");
         } catch (NonUniqueUsernameException | FullLobbyException e) {
@@ -108,7 +108,7 @@ class LobbyTest {
 
     @Test
     void getRequiredPlayers() {
-        Lobby lobby = new Lobby(1, "player1", 2);
+        Lobby lobby = new Lobby("NameID", "player1", 2);
         assertEquals(2, lobby.getRequiredPlayers());
     }
 }
