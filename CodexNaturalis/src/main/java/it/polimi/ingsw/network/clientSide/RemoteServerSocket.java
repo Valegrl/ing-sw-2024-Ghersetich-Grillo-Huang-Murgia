edecc54e0fe5bb594dd44f9450109a24793faf02
filippomurgia <filippo.murgia@mail.polimi.it>
2024-04-29 17:44:44 +0200

    Started implementing RemoteClientSocket,RemoteServerSocket

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/clientSide/RemoteServerSocket.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/clientSide/RemoteServerSocket.java
@@ -1,18 +1,55 @@
 package it.polimi.ingsw.network.clientSide;
 
+import com.google.gson.Gson;
 import it.polimi.ingsw.eventUtils.event.Event;
 import it.polimi.ingsw.network.Client;
 import it.polimi.ingsw.network.Server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.rmi.RemoteException;
 
 public class RemoteServerSocket implements Server {
+    private final Socket socket;
+    private final ObjectOutputStream outputStream;
+    private final ObjectInputStream inputStream;
+    private RemoteServerSocket(String ipAddress, int portNumber) throws IOException {
+        try {
+            this.socket= new Socket(ipAddress,portNumber);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+            this.inputStream = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     @Override
     public void direct(Event event, Client client) throws RemoteException {
-
+        //TODO Figure out how to use client parameter
+        try {
+            Gson gson = new Gson();
+            String jsonString = gson.toJson(event);
+            outputStream.writeObject(jsonString);
+            outputStream.flush();
+        } catch (IOException e) {
+            System.err.println("I/O error: " + e.getMessage());
+        }
     }
-
-    void readStream(){
-
+    public void readStream() throws RemoteException{
+        try {
+            String jsonString = (String) inputStream.readObject();
+            Gson gson = new Gson();
+            Event event;
+            event = gson.fromJson(jsonString, Event.class);
+        } catch (IOException e) {
+            System.err.println("I/O error: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println("Class non found: " + e.getMessage());
+        }
     }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/serverSide/RemoteClientSocket.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/serverSide/RemoteClientSocket.java
@@ -1,17 +1,50 @@
 package it.polimi.ingsw.network.serverSide;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
 import it.polimi.ingsw.eventUtils.event.Event;
 import it.polimi.ingsw.network.Client;
+import it.polimi.ingsw.network.Server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.rmi.RemoteException;
-
+import java.net.Socket;
 public class RemoteClientSocket implements Client {
+    private Server server;
+    private final ObjectOutputStream outputStream;
+    private final ObjectInputStream inputStream;
+    private RemoteClientSocket(Socket socket, Server server) throws RemoteException {
+        this.server=server;
+        try {
+            this.outputStream = new ObjectOutputStream(socket.getOutputStream());
+            this.inputStream = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            throw new RemoteException();
+        }
+    }
     @Override
     public void report(Event event) throws RemoteException {
-
+        try {
+            Gson gson = new Gson();
+            String jsonString = gson.toJson(event);
+            outputStream.writeObject(jsonString);
+            outputStream.flush();
+        } catch (IOException e) {
+            System.err.println("I/O error: " + e.getMessage());
+        }
     }
-
     void readStream(){
-
+        try {
+            String jsonString = (String) inputStream.readObject();
+            Gson gson = new Gson();
+            Event event;
+            event = gson.fromJson(jsonString, Event.class);
+        } catch (IOException e) {
+            System.err.println("I/O error: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println("Class not found: " + e.getMessage());
+        }
     }
 }
