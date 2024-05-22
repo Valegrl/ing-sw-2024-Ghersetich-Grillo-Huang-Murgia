bae25ca098677c1e03999b33cc3938f80b9bff07
filippomurgia <filippo.murgia@mail.polimi.it>
2024-05-22 23:32:56 +0200

    Lobby fxml and controller fix

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/controller/InLobbyController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/controller/InLobbyController.java
@@ -2,116 +2,81 @@ package it.polimi.ingsw.view.gui.controller;
 
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
-import java.util.Random;
-import java.util.ResourceBundle;
-
-
-public class inLobbyController {
-
-
+public class InLobbyController {
     @FXML
     private TextField chatInput;
-
 
     @FXML
     private TextArea chatSpace;
 
-
     @FXML
     private Text chatText;
-
 
     @FXML
     private HBox hBox;
 
-
     @FXML
     private Text lobbyText;
-
 
     @FXML
     private AnchorPane mainAnchor;
 
-
     @FXML
     private AnchorPane playerAnchor1;
-
 
     @FXML
     private AnchorPane playerAnchor2;
 
-
     @FXML
     private AnchorPane playerAnchor3;
-
 
     @FXML
     private ImageView playerImage1;
 
-
     @FXML
     private ImageView playerImage2;
-
 
     @FXML
     private ImageView playerImage3;
 
-
     @FXML
     private Text playerText1;
-
 
     @FXML
     private Text playerText2;
 
-
     @FXML
     private Text playerText3;
-
 
     @FXML
     private Button readyButton;
 
-
     @FXML
     private AnchorPane selfAnchor;
-
 
     @FXML
     private ImageView selfImage;
 
-
     @FXML
     private Text selfText;
-
 
     @FXML
     private Button showReady1;
 
-
     @FXML
     private Button showReady2;
 
-
     @FXML
     private Button showReady3;
-
-
 
 /*
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -122,18 +87,14 @@ public class inLobbyController {
     }
     */
 
-
     /**
      * Method to set the name of the lobby
      *
      * @param lobbyName the name of the lobby
      */
-
-
     public void setLobbyName(String lobbyName) {
         lobbyText.setText("LOBBY: " + lobbyName);
     }
-
 
     /**
      * Method to dynamically set the information of the player in its box,
@@ -145,7 +106,6 @@ public class inLobbyController {
         selfText.setText(username);
     }
 
-
     /**
      * Method to dynamically set the information of the other players that entered the lobby,
      * visualizing their username, an image, and if they are ready or not.
@@ -155,10 +115,8 @@ public class inLobbyController {
         if(hBox.getChildren().size() == 1){
             playerText1.setText(username);
 
-
             //if player is ready, show the text ready
             //showReady1.setText("Ready");
-
 
             playerAnchor1.setVisible(true);
             playerAnchor1.setManaged(true);
@@ -166,10 +124,8 @@ public class inLobbyController {
         if(hBox.getChildren().size() == 2){
             playerText2.setText(username);
 
-
             //if player is ready, show the text ready
             //showReady2.setText("Ready");
-
 
             playerAnchor2.setVisible(true);
             playerAnchor2.setManaged(true);
@@ -177,19 +133,14 @@ public class inLobbyController {
         if(hBox.getChildren().size() == 3){
             playerText3.setText(username);
 
-
             //if player is ready, show the text ready
             //showReady3.setText("Ready");
-
 
             playerAnchor3.setVisible(true);
             playerAnchor3.setManaged(true);
         }
     }
 
-
     //TODO chat implementation
-
-
 }
 
