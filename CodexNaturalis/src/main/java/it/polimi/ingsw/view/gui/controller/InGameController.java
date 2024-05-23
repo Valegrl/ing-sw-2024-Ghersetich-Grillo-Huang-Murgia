49f06184124d527040695fe0c0c8f6fead4919b3
filippomurgia <filippo.murgia@mail.polimi.it>
2024-05-23 13:59:56 +0200

    InGame controller and fxml

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/controller/InGameController.java
@@ -0,0 +1,264 @@
+package it.polimi.ingsw.view.gui.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+
+public class InGameController {
+
+
+    @FXML
+    private Label animalOcc1;
+
+
+    @FXML
+    private Label animalOcc2;
+
+
+    @FXML
+    private Label animalOcc3;
+
+
+    @FXML
+    private Label animalOcc4;
+
+
+    @FXML
+    private ComboBox<?> chatChoice;
+
+
+    @FXML
+    private TextField chatMessage;
+
+
+    @FXML
+    private TextArea chatText;
+
+
+    @FXML
+    private Text chatTitle;
+
+
+    @FXML
+    private ImageView common1;
+
+
+    @FXML
+    private ImageView common2;
+
+
+    @FXML
+    private Text commonText;
+
+
+    @FXML
+    private Label fungiOcc1;
+
+
+    @FXML
+    private Label fungiOcc2;
+
+
+    @FXML
+    private Label fungiOcc3;
+
+
+    @FXML
+    private Label fungiOcc4;
+
+
+    @FXML
+    private ImageView gold1;
+
+
+    @FXML
+    private ImageView gold2;
+
+
+    @FXML
+    private ImageView goldDeck;
+
+
+    @FXML
+    private Text goldText;
+
+
+    @FXML
+    private ImageView hand1;
+
+
+    @FXML
+    private ImageView hand2;
+
+
+    @FXML
+    private ImageView hand3;
+
+
+    @FXML
+    private Text handText;
+
+
+    @FXML
+    private Label inkwellOcc1;
+
+
+    @FXML
+    private Label inkwellOcc2;
+
+
+    @FXML
+    private Label inkwellOcc3;
+
+
+    @FXML
+    private Label inkwellOcc4;
+
+
+    @FXML
+    private Label insectOcc1;
+
+
+    @FXML
+    private Label insectOcc2;
+
+
+    @FXML
+    private Label insectOcc3;
+
+
+    @FXML
+    private Label insectOcc4;
+
+
+    @FXML
+    private Label lobbyName;
+
+
+    @FXML
+    private Label manuscriptOcc1;
+
+
+    @FXML
+    private Label manuscriptOcc2;
+
+
+    @FXML
+    private Label manuscriptOcc3;
+
+
+    @FXML
+    private Label manuscriptOcc4;
+
+
+    @FXML
+    private Label plantOcc1;
+
+
+    @FXML
+    private Label plantOcc2;
+
+
+    @FXML
+    private Label plantOcc3;
+
+
+    @FXML
+    private Label plantOcc4;
+
+
+    @FXML
+    private ComboBox<?> playAreaChoice;
+
+
+    @FXML
+    private Label quillOcc1;
+
+
+    @FXML
+    private Label quillOcc2;
+
+
+    @FXML
+    private Label quillOcc3;
+
+
+    @FXML
+    private Label quillOcc4;
+
+
+    @FXML
+    private ImageView resource1;
+
+
+    @FXML
+    private ImageView resource2;
+
+
+    @FXML
+    private ImageView resourceDeck;
+
+
+    @FXML
+    private Text resourceText;
+
+
+    @FXML
+    private ImageView secret;
+
+
+    @FXML
+    private Text secretText;
+
+
+    @FXML
+    private Button sendButton;
+
+
+    @FXML
+    private AnchorPane uncovered1;
+
+
+    @FXML
+    private AnchorPane uncovered2;
+
+
+    @FXML
+    private AnchorPane uncovered3;
+
+
+    @FXML
+    private AnchorPane uncovered4;
+
+
+    @FXML
+    private Text uncoveredUsername1;
+
+
+    @FXML
+    private Text uncoveredUsername2;
+
+
+    @FXML
+    private Text uncoveredUsername3;
+
+
+    @FXML
+    private Text uncoveredUsername4;
+
+
+    @FXML
+    private VBox vBox;
+
+
+}
+
