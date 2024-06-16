20434d4e3af45e9d4b3c300b404b6ddbe0ca08c9
filippomurgia <filippo.murgia@mail.polimi.it>
2024-06-16 16:09:15 +0200

    Reconnect scene

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/controller/ReconnectController.java
@@ -0,0 +1,51 @@
+package it.polimi.ingsw.view.gui.controller;
+
+import it.polimi.ingsw.eventUtils.event.fromView.Feedback;
+import it.polimi.ingsw.view.FXMLController;
+import it.polimi.ingsw.view.View;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+
+public class ReconnectController extends FXMLController {
+
+    @FXML
+    private Label gameLabel;
+
+    @FXML
+    private AnchorPane mainAnchor;
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    private Label reconnectLabel;
+
+    @FXML
+    private AnchorPane upperAnchor;
+
+    public ReconnectController(){
+        super();
+    }
+
+    @Override
+    public void run(View view, Stage stage){
+        this.view = view;
+        this.stage = stage;
+        this.controller = view.getController();
+
+
+    }
+
+    @Override
+    public void handleResponse(Feedback feedback, String message, String eventID) {
+
+    }
+
+}
+
+
